c40643146586c3aaba5483ee58f62101261a0185
Lily <30967156+uqtdev@users.noreply.github.com>
2024-05-27 00:37:46 -0400

    Scripts Actually work this time. 100%. Will be expanding configurations added to this aswell as distros supported as I explore more

--- full_setup.sh
+++ full_setup.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+	echo "                                               0               "
+	echo "    -        *   ]          -     *                    .       "
+	echo " r  0                                              .        r  "
+	echo "     .        THIS SCRIPT MUST BE RAN AS ROOT           =      "
+	echo "                                                               "
+	echo "  .    0                =              -        r        [     "
+	echo "              .               ]                                "
+	exit 1
+fi
+
+USERNAME="$sudo_user"
+
+export USERNAME
+
 echo "Running Package Installer.."
-packages.sh
+sudo -E bash packages.sh
 echo "Packages installer script finished! Running Symlinks Script.."
-symlinks.sh
+sudo -E bash symlinks.sh
 echo "Symlinks finished!"
 
 echo "Running bitwarden.."
-sudo -u "$SUDO_USER" nohup flatpak run com.bitwarden.desktop > /dev/null 2>&1 &
+sudo -u "$USERNAME" nohup flatpak run com.bitwarden.desktop > /dev/null 2>&1 &
 echo "Bitwarden running. Go crazy!"
--- symlinks.sh
+++ symlinks.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-HOME_PATH="/home/$SUDO_USER"
+HOME_PATH="/home/$USERNAME"
 links=".zshrc, .gitconfig"
 
 echo "creating symlinks"
 for file in $links; do
 	ln -s "$(pwd)/$file" "$HOME_PATH/$file"
 done
+
+
 
