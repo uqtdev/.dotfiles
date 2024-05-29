bdcadf4de4532c3cce3d885728ccc81bd2100e85
ohmygodmisosoup <uqtdev@fedora.sasi.zcs.k12.in.us>
2024-05-29 10:02:31 -0400

    Fixed Symlinks

--- symlinks.sh
+++ symlinks.sh
@@ -4,12 +4,12 @@ SOURCE_DIR="$(pwd)/user"
 TARGET_DIR="$(dirname "$(pwd)")"
 CONF=$TARGET_DIR/.config/
 
-ln -nf conf/waybar $CONF/waybar/
-ln -nf conf/kitty $CONF/kitty/
-ln -nf conf/sway $CONF/sway/
-ln -nf conf/rofi $CONF/rofi/
-ln -nf conf/.gitconfig $TARGET_DIR/.gitconfig
-ln -nf conf/.zshrc $TARGET_DIR/.zshrc
+ln -sf conf/waybar $CONF/waybar/
+ln -sf conf/kitty $CONF/kitty/
+ln -sf conf/sway $CONF/sway/
+ln -sf conf/rofi $CONF/rofi/
+ln -sf conf/.gitconfig $TARGET_DIR/.gitconfig
+ln -sf conf/.zshrc $TARGET_DIR/.zshrc
 #ln -nf conf/vesktop/settings/settings.json $conf/vesktop/settings/settings.json
 
 echo "All files have been linked."
