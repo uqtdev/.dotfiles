98a814b69ea51049fe154ba399542cf6a815c340
ohmygodmisosoup <30967156+uqtdev@users.noreply.github.com>
2024-05-29 16:44:43 -0400

    Fixed symlinks.. again... don't tell anyone, but I really suck at them

--- symlinks.sh
+++ symlinks.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 SOURCE_DIR="$(pwd)/user"
-TARGET_DIR="$(dirname "$(pwd)")"
-CONF=$TARGET_DIR/.config/
+HOME_DIR="$(dirname "$(pwd)")"
+CONF=$HOME_DIR/.config
 
-ln -sf conf/waybar $CONF/waybar/
-ln -sf conf/kitty $CONF/kitty/
-ln -sf conf/sway $CONF/sway/
-ln -sf conf/rofi $CONF/rofi/
-ln -sf conf/.gitconfig $TARGET_DIR/.gitconfig
-ln -sf conf/.zshrc $TARGET_DIR/.zshrc
+echo "HOME_DIR registered as $HOME_DIR"
+
+ln -nf conf/waybar/config $CONF/waybar/config
+ln -nf conf/waybar/style.css $CONF/waybar/style.css
+ln -nf conf/kitty/kitty.conf $CONF/kitty/kitty.conf
+ln -nf conf/sway/config $CONF/sway/config
+ln -nf conf/rofi/config.rasi $CONF/rofi/config.rasi
+ln -nf conf/.gitconfig $HOME_DIR/.gitconfig
+ln -nf conf/.zshrc $HOME_DIR/.zshrc
 #ln -nf conf/vesktop/settings/settings.json $conf/vesktop/settings/settings.json
 
 echo "All files have been linked."
