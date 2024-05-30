3784da9375318cb344f789d2b2c53b59c3353b97
Lily <30967156+uqtdev@users.noreply.github.com>
2024-05-30 00:11:54 -0400

    Fixed (Again) Symlinks (Again) and Started on bin folder and improved install script and overall all better scripts

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/user/bin/env bash
+
+blue='\e[1;34m'
+red='\e[1;31m'
+white='\e[0;37m'
+dotfiles_repo_dir=$(pwd)
+dotfiles_home_dir=(.zshrc .gitconfig)
+dotfiles_xdg_config_dir=()
--- symlinks.sh
+++ symlinks.sh
@@ -6,6 +6,7 @@ CONF=$HOME_DIR/.config
 
 echo "HOME_DIR registered as $HOME_DIR"
 
+mkdir -p $CONF/waybar $CONF/kitty $CONF/sway/ $CONF/rofi
 ln -nf conf/waybar/config $CONF/waybar/config
 ln -nf conf/waybar/style.css $CONF/waybar/style.css
 ln -nf conf/kitty/kitty.conf $CONF/kitty/kitty.conf
