b6641e802971da0a7d6b8111f207a9f21cbfed10
ohmygodmisosoup <30967156+uqtdev@users.noreply.github.com>
2024-05-31 11:44:49 -0400

    Further worked on sway config and made slight additions to future symlinks script

--- bin/symlinks.sh
+++ bin/symlinks.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-# Define variables
+# Variables
 DOTFILES_DIR="$HOME/.dotfiles"
-SHARE_DIR="$HOME/.local/share/"
+CONF_DIR="$HOME/.config"
+SHARE_DIR="$HOME/.local/share"
 ICON_THEME_NAME="BeautySolar"
 CURSOR_THEME_NAME="Bibata-Original-Ice"
-ICON_THEME_DIR="$DOTFILES_DIR/themeing/icons/"
-CURSOR_THEME_DIR="$DOTFILES_DIR/themeing/cursors/"
+ICON_THEME_DIR="$DOTFILES_DIR/themeing/icons"
+CURSOR_THEME_DIR="$DOTFILES_DIR/themeing/cursors"
 
 # Create destination directory if it doesn't exist
 #mkdir -p "$SHARE_DIR/icons/$ICON_THEME_NAME"
 #sudo ln -sfn "$CURSOR_THEME_DIR"/ "$SHARE_DIR/$CURSOR_THEME_NAME"/
-sudo ln -sfn "$ICON_THEME_DIR/$ICON_THEME_NAME"/ "$SHARE_DIR/icons/$ICON_THEME_NAME"
+ln -sf "$ICON_THEME_DIR/$ICON_THEME_NAME"/ "$SHARE_DIR/icons/$ICON_THEME_NAME"
 
